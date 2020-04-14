520290428c7662640b09ba10e734dd2c067432ef
fumitako <33313920+fumitako@users.noreply.github.com>
2020-04-15 00:30:22 +0900

    Update SieveOfEratosthenes.py

--- SieveOfEratosthenes.py
+++ SieveOfEratosthenes.py
@@ -1,12 +1,8 @@
-import numpy as np
-limit = 10**5
-def Eratosthenes(limit):
-	is_prime = np.full(limit+1, True)
-	is_prime[:2] = False
-	for p in range(2,limit+1):
-		if p**2 > limit:
-			break
-		if is_prime[p]:
-			is_prime[p**2::p] = False
-
-	return np.arange(limit+1)[is_prime]
+def eratosthenes(N):
+	is_prime = [True] * (N + 1)
+	is_prime[0], is_prime[1] = False, False
+	for i in range(2, int(N**(1/2)) + 1):
+		if is_prime[i]:
+			for j in range(i * 2, N + 1, i):
+				is_prime[j] = False
+	return [i for i in range(N+1) if is_prime[i]]
